3c18049958f67dc7489d42a5f2993dc0b25e6ddb
marcus.titze <marcus.titze@localhost>
2008-07-23 11:10:35 +0000

    umstallung auf jquery

---
+++ htdocs/static/js/seishinkan.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(":text").focus(function() {
+    $(this).select();
+  });
+
+  $("#id_username").focus();
+
+  $("#search-clear").click(function() {
+    $(":text").val("");
+  });
+
+  $("#language").change(function() {
+    $("#setlang").submit();
+  });
+
+  $("#login-button").click(function() {
+    $("#login-button").attr("disabled", "disabled");
+    $("#login-form").submit();
+  });
+
+});
