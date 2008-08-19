51ffc9212a8104daa4a370f5b431d96f8cb76914
marcus.titze <marcus.titze@localhost>
2008-08-19 21:33:27 +0000

    mitgliederbereich, email verteiler

--- src/seishinkan/website/templatetags/seishinkan_tags.py
+++ src/seishinkan/website/templatetags/seishinkan_tags.py
@@ -27,3 +27,13 @@ def extension( value ):
     except:
         return value
 
+@register.filter
+def age( value ):
+    today = date.today()
+    try:
+        this_years_birthday = date( today.year, value.month, value.day )
+        if this_years_birthday <= today:
+            return today.year - value.year
+        return today.year - value.year - 1
+    except:
+        return ''
