6279e4e02d58b07f3329e009b5fec4078c337df0
marcus.titze <marcus.titze@localhost>
2008-08-19 00:16:39 +0000

    members import

--- seishinkan/src/seishinkan/members/admin.py
+++ seishinkan/src/seishinkan/members/admin.py
@@ -7,16 +7,26 @@ class PersonAdmin( admin.ModelAdmin ):
     ordering = ['vorname', 'nachname']
     list_display = ( 'vorname', 'nachname', 'public', 'modified', 'id' )
     list_display_links = ( 'vorname', 'nachname' )
+    list_per_page = 200
+    search_fields = ( 'vorname', 'nachname', 'email', 'text' )
+
+class GraduierungAdmin( admin.ModelAdmin ):
+    ordering = ['id']
+    list_display = ( 'person', 'datum', 'graduierung', 'public', 'modified', 'id' )
 
 class GraduierungInline( admin.TabularInline ):
     model = Graduierung
 
 class MitgliedAdmin( admin.ModelAdmin ):
-    ordering = ['vorname', 'nachname']
-    list_display = ( 'vorname', 'nachname', 'status', 'public', 'modified', 'id' )
+    ordering = ['id']
+    list_display = ( 'id', 'vorname', 'nachname', 'aktuelle_graduierung', 'status', 'email', 'geburt', 'mitglied_seit', 'ist_vorstand', 'ist_trainer', 'ist_kind' )
     list_display_links = ( 'vorname', 'nachname' )
+    list_filter = ( 'status', 'ist_vorstand', 'ist_trainer', 'ist_kind')
+    list_per_page = 200
+    search_fields = ( 'vorname', 'nachname', 'email', 'text' )
     inlines = [ GraduierungInline, ]
 
 admin.site.register( Person, PersonAdmin )
 admin.site.register( Mitglied, MitgliedAdmin )
+admin.site.register( Graduierung, GraduierungAdmin )
 
