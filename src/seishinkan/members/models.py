c8c9c20df3e1f22eee08bc5b238e8f13f0621cb2
marcus.titze <marcus.titze@localhost>
2008-08-19 00:16:39 +0000

    members import

--- src/seishinkan/members/models.py
+++ src/seishinkan/members/models.py
@@ -2,7 +2,9 @@
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from seishinkan.utils import DEFAULT_MAX_LENGTH, AbstractModel
+from seishinkan.utils import DEFAULT_MAX_LENGTH, AbstractModel, UnicodeReader
+from datetime import datetime
+import csv
 
 STATUS = [
     ( 0, _( u'Austritt' ) ),
@@ -49,23 +51,34 @@ class Person( AbstractModel ):
     name.allow_tags = True
 
     def __unicode__( self ):
-        return self.name()
+        return '%s (%s)' % ( self.name(), self.id )
 
     class Meta:
         ordering = ['vorname', 'nachname']
         verbose_name = _( u'Person' )
         verbose_name_plural = _( u'Personen' )
 
+class MitgliederManager( models.Manager ):
+    def get_query_set( self ):
+        return super( MitgliederManager, self ).get_query_set().filter( public = True )
+
 class Mitglied( Person ):
     status = models.IntegerField( _( u'Status' ), default = 1, choices = STATUS )
-    nummer = models.IntegerField( _( u'Nummer' ) )
     mitglied_seit = models.DateField( _( u'Mitglied seit' ), blank = True, null = True )
     ist_vorstand = models.BooleanField( _( u'Vorstand' ), default = False )
     ist_trainer = models.BooleanField( _( u'Trainer' ), default = False )
     ist_kind = models.BooleanField( _( u'Kind' ), default = False )
 
+    objects = models.Manager()
+    public_objects = MitgliederManager()
+
+    def aktuelle_graduierung( self ):
+        return self.graduierung_set.latest( 'datum' )
+    aktuelle_graduierung.short_description = _( u'Graduierung' )
+    aktuelle_graduierung.allow_tags = True
+
     def __unicode__( self ):
-        return Person.__unicode__()
+        return super( Mitglied, self ).__unicode__()
 
     class Meta( Person.Meta ):
         verbose_name = _( u'Mitglied' )
@@ -73,7 +86,7 @@ class Mitglied( Person ):
 
 class Graduierung( AbstractModel ):
     person = models.ForeignKey( Person, verbose_name = _( u'Person' ) )
-    datum = models.DateField( _( u'Datum' ) )
+    datum = models.DateField( _( u'Datum' ), blank = True, null = True )
     graduierung = models.IntegerField( _( u'Graduierung' ), choices = GRADUIERUNGEN )
     text = models.TextField( _( u'Text' ), blank = True )
 
@@ -84,3 +97,71 @@ class Graduierung( AbstractModel ):
         ordering = ['graduierung']
         verbose_name = _( u'Graduierung' )
         verbose_name_plural = _( u'Graduierungen' )
+
+def import_mitglieder( filename = 'members/mitglieder.csv'):
+    for row in UnicodeReader( open( filename, 'rb' ) ):
+        m = Mitglied()
+        m.public = True
+        m.creation = datetime.now().strftime( '%Y-%m-%d %H:%M:%S' )
+        m.modified = datetime.now().strftime( '%Y-%m-%d %H:%M:%S' )
+        m.id = row[0]
+        m.nachname = row[1]
+        m.vorname = row[2]
+        m.plz = row[3]
+        m.stadt = row[4]
+        m.strasse = row[5]
+        m.fon = row[6]
+        m.mobil = row[7]
+        m.fax = row[8]
+        m.email = row[9]
+
+        if 'j' == row[14].lower().strip():
+            m.status = 2 # Passiv
+
+        if 'n' == row[15].lower().strip():
+            m.status = 0 # Austritt
+
+        if 'j' == row[17].lower().strip():
+            m.ist_vorstand = True
+
+        if 'j' == row[18].lower().strip():
+            m.ist_trainer = True
+
+        if 'j' == row[19].lower().strip():
+            m.ist_kind = True
+
+        try:
+            m.geburt = datetime.strptime( row[10], '%d.%m.%Y' ).strftime( '%Y-%m-%d' )
+        except:
+            pass
+
+        try:
+            m.mitglied_seit = datetime.strptime( row[11], '%d.%m.%Y' ).strftime( '%Y-%m-%d' )
+        except:
+            pass
+
+        m.save()
+        print m
+
+
+        # Graduierungen...
+
+
+
+        if row[12]:
+            for gid, grad in GRADUIERUNGEN:
+                if grad == row[12]:
+                    try:
+                        gdatum = datetime.strptime( row[13], '%d.%m.%Y' ).strftime( '%Y-%m-%d' )
+                    except:
+                        print m
+                        gdatum = None
+                
+                    g, created = Graduierung.objects.get_or_create( person = m, graduierung = gid, datum = gdatum,
+                                                                    defaults = {
+                            'person': m,
+                            'datum': gdatum,
+                            'graduierung': gid,
+                            })
+
+                    g.save()
