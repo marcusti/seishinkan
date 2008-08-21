c196c9c4162b6cddddda58dc9d8a79d83c45319e
marcus.titze <marcus.titze@localhost>
2008-08-21 11:55:54 +0000

    austritt datum

--- src/seishinkan/members/admin.py
+++ src/seishinkan/members/admin.py
@@ -15,9 +15,9 @@ class GraduierungInline( admin.TabularInline ):
 
 class MitgliedAdmin( admin.ModelAdmin ):
     ordering = ['id']
-    list_display = ( 'id', 'vorname', 'nachname', 'aktuelle_graduierung', 'status', 'email', 'alter', 'modified' )
+    list_display = ( 'id', 'vorname', 'nachname', 'aktuelle_graduierung', 'status', 'email', 'alter', 'mitglied_seit' )
     list_display_links = ( 'vorname', 'nachname' )
-    list_filter = ( 'status', 'ist_vorstand', 'ist_trainer', 'ist_kind')
+    list_filter = ( 'status', 'ist_vorstand', 'ist_trainer', 'ist_kind', 'mitglied_seit')
     list_per_page = 200
     search_fields = ( 'vorname', 'nachname', 'email', 'text' )
     inlines = [ GraduierungInline, ]
