920a082fa8663faf1bdb64d1feffde7c72ef4903
marcus.titze <marcus.titze@localhost>
2008-09-03 06:36:19 +0000

    js reaktivierung

---
+++ seishinkan/htdocs/static/js/seishinkan.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$(":text").focus(function() {
+		$(this).select();
+	    });
+
+	$("#id_username").focus();
+
+	$("#search-clear").click(function() {
+		$(":text").val("");
+	    });
+
+	$("#language").change(function() {
+		$("#setlang").submit();
+	    });
+
+	$("#login-button").click(function() {
+		$("#login-button").attr("disabled", "disabled");
+		$("#login-form").submit();
+	    });
+
+	$("#kontakt-button").click(function() {
+		$("#kontakt-button").attr("disabled", "disabled");
+		$("#kontakt-form").submit();
+	    });
+
+    });
+
+var RecaptchaOptions = {
+    theme : 'clean',
+    lang : 'de'
+};
