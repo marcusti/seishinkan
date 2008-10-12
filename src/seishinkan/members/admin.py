f8d688ef1214b67e51bbbafac303d16e6461781b
marcus.titze <marcus.titze@localhost>
2008-10-12 13:31:18 +0000

    l?nder

--- src/seishinkan/members/admin.py
+++ src/seishinkan/members/admin.py
@@ -3,6 +3,10 @@
 from django.contrib import admin
 from seishinkan.members.models import *
 
+class LandAdmin( admin.ModelAdmin ):
+    ordering = ['name']
+    list_display = ( 'name', 'id' )
+
 class GraduierungAdmin( admin.ModelAdmin ):
     ordering = ['-graduierung', '-datum', 'person']
     list_display = ( 'person', 'datum', 'graduierung', 'public', 'modified', 'id' )
@@ -17,10 +21,11 @@ class MitgliedAdmin( admin.ModelAdmin ):
     ordering = ['id']
     list_display = ( 'id', 'vorname', 'nachname', 'aktuelle_graduierung', 'status', 'email', 'alter', 'mitglied_seit' )
     list_display_links = ( 'vorname', 'nachname' )
-    list_filter = ( 'status', 'ist_vorstand', 'ist_trainer', 'ist_kind', 'mitglied_seit')
+    list_filter = ( 'status', 'land', 'ist_vorstand', 'ist_trainer', 'ist_kind', 'mitglied_seit')
     list_per_page = 200
     search_fields = ( 'vorname', 'nachname', 'email', 'text' )
     inlines = [ GraduierungInline, ]
 
 admin.site.register( Mitglied, MitgliedAdmin )
 admin.site.register( Graduierung, GraduierungAdmin )
+admin.site.register( Land, LandAdmin )
