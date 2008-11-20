b09a9596dd058c635273b6d20b63f7dd4777d742
marcus.titze <marcus.titze@localhost>
2008-11-20 16:46:26 +0000

    email unit tests

--- src/seishinkan/website/tests.py
+++ src/seishinkan/website/tests.py
@@ -1,11 +1,35 @@
 #-*- coding: utf-8 -*-
 
+from django.core import mail
 from django.test import TestCase
+from seishinkan import settings
 from seishinkan.website.models import Seite
-import unittest
 
 NAME_START = 'Start'
 NAME_SUB = 'Sub'
+
+class EmailTest( TestCase ):
+    def setUp( self ):
+        self.SUBJECT = 'Hallo'
+        self.BODY = 'Django JUnit Email Test'
+
+    def testAdminEmail( self ):
+        mail.mail_admins( self.SUBJECT, self.BODY )
+        self.assertEquals( len( mail.outbox ), 1 )
+        self.assertEquals( mail.outbox[0].subject, settings.EMAIL_SUBJECT_PREFIX + self.SUBJECT )
+
+    def testStratoEmail( self ):
+        con = mail.SMTPConnection( host = settings.STRATO_EMAIL_HOST, username = settings.STRATO_EMAIL_HOST_USER, password = settings.STRATO_EMAIL_HOST_PASSWORD, use_tls = settings.STRATO_EMAIL_USE_TLS )
+        self.failIf( con is None )
+
+        email = mail.EmailMessage()
+        email.subject = self.SUBJECT
+        email.body = self.BODY
+        email.from_email = settings.TEST_EMAIL
+        email.to = [ settings.TEST_EMAIL, settings.ADMINS[0][1] ]
+        email.connection = con
+        email.send()
+        self.assertEquals( len( mail.outbox ), 1 )
 
 class SeiteTest( TestCase ):
     def setUp( self ):
