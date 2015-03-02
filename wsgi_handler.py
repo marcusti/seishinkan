5cf78c44845e0ae1d9cd9b6cfa8e8d8d87bede23
Marcus Titze <marcus.titze@gmail.com>
2015-03-02 07:06:06 +0100

    virtualenv

--- wsgi_handler.py
+++ wsgi_handler.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+sys.path.append('/home/marcus/env/seishinkan/lib/python2.7/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'seishinkan.settings'
 
 import django.core.handlers.wsgi
